cd68ef0f7c5af49c369da34184d8c3ad8c789355
Umut Can KOCAMAN <33186542+uckocaman@users.noreply.github.com>
2020-11-24 14:22:31 +0300

    Add files via upload

---
+++ Data Modelling/db_modelling_synonym_script.sql
@@ -0,0 +1,5 @@
+--Oluþturmuþ olduðunuz tablolarý kaynak olarak alan bir adet synonym, bir adet view scripti oluþturunuz.
+
+CREATE OR REPLACE SYNONYM musteriler for week2.customer;
+CREATE OR REPLACE SYNONYM siparis for week2.orders;
+CREATE OR REPLACE SYNONYM siparisDetaylari for week2.order_details;
